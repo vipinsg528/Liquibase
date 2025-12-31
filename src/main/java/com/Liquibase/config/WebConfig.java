ce02a5ba81a0ea45bf3ccd32e4c3e87a6725fa7b
vipinsg528 <vipinsg.bel@gmail.com>
2025-12-31 08:45:04 +0530

    updated indentation in WebConfig.java file.

--- src/main/java/com/Liquibase/config/WebConfig.java
+++ src/main/java/com/Liquibase/config/WebConfig.java
@@ -17,18 +17,11 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedMethods(List.of(
-                "GET",
-                "POST",
-                "PUT",
-                "DELETE",
-                "OPTIONS"
-        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
         source.registerCorsConfiguration("/**", config);
         return source;
